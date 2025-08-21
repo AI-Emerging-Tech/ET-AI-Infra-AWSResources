6af1e1a842917437b4b0de724721ed3403e94f9c
Dheeraj Bhupathiraju <dheerajvarma.bhupathiraju@valuemomentum.com>
2025-08-21 16:32:07 -0400

    Created cd user and provided permissions to S3 and dynamo DB services

---
+++ infra/deploy/variables.tf
@@ -0,0 +1,50 @@
+variable "prefix" {
+  description = "Prefix for resources in AWS"
+  default     = "vamet"
+}
+variable "project" {
+  description = "Project name for taggigng resources"
+  default     = "devops-iac"
+}
+variable "contact" {
+  description = "contact email for tagging resources"
+  default     = "dheerajvarma.bhupathiraju@valuemomentum.com"
+}
+
+# variable "db_username" {
+#   description = "Username for the recipe app api database"
+#   default     = "idpdbuser"
+# }
+
+# variable "db_password" {
+#   description = "Password for the Terraform database"
+# }
+
+# variable "ecr_proxy_image" {
+#   description = "Path to the ECR repo with the proxy image"
+# }
+
+# variable "ecr_app_image" {
+#   description = "Path to the ECR repo with the API image"
+# }
+
+# variable "django_secret_key" {
+#   description = "Secret key for Django"
+# }
+
+# variable "dns_zone_name" {
+#   description = "Domain name"
+#   default     = "shribhavanifarm.com"
+# }
+
+# variable "subdomain" {
+#   description = "Subdomain for each environment"
+#   type        = map(string)
+
+#   default = {
+#     prod    = "api"
+#     staging = "api.staging"
+#     dev     = "api.dev"
+#   }
+# }
+
