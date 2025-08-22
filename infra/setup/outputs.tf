beee6ff0787ff4816d6ec9a0f5629e04de622d1d
Dheeraj Bhupathiraju <dheerajvarma.bhupathiraju@valuemomentum.com>
2025-08-22 06:12:58 -0400

    ECR Repository provisioned

--- infra/setup/outputs.tf
+++ infra/setup/outputs.tf
@@ -8,15 +8,15 @@ output "cd_user_access_key_secret" {
   value       = aws_iam_access_key.cd.secret
   sensitive   = true
 }
-# output "ecr_repo_app" {
-#   description = "ECR repository URL for app image"
-#   value       = aws_ecr_repository.app.repository_url
-# }
+output "ecr_repo_app" {
+  description = "ECR repository URL for app image"
+  value       = aws_ecr_repository.app.repository_url
+}
 
-# output "ecr_repo_proxy" {
-#   description = "ECR repository URL for the proxy image"
-#   value       = aws_ecr_repository.proxy.repository_url
-# }
+output "ecr_repo_proxy" {
+  description = "ECR repository URL for the proxy image"
+  value       = aws_ecr_repository.proxy.repository_url
+}
 
 # output "cd_user_name" {
 #   description = " printion the aws IAM cd user name"
