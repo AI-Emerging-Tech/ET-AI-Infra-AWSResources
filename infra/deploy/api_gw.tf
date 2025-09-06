0a77d58a296687b6a3b233a1b9296f12e4f138b2
Dheeraj Bhupathiraju <dheerajvarma.bhupathiraju@valuemomentum.com>
2025-09-06 18:40:08 -0400

    Updated  ARN definition in main.tf

--- infra/deploy/api_gw.tf
+++ infra/deploy/api_gw.tf
@@ -41,7 +41,9 @@ resource "aws_lambda_permission" "apigw-lambda" {
   function_name = aws_lambda_function.lambda_function.function_name
   principal     = "apigateway.amazonaws.com"
   # source_arn    = "arn:aws:execute-api:${data.aws_region.current.name}:${data.aws_region.current.id}:${aws_api_gateway_rest_api.API.id}/*/${aws_api_gateway_method.Method.http_method}${aws_api_gateway_resource.Resource.path}"
-  source_arn = "arn:aws:execute-api:*:*:${aws_api_gateway_rest_api.API.id}/*/${aws_api_gateway_method.Method.http_method}${aws_api_gateway_resource.Resource.path}"
+  # source_arn = "arn:aws:execute-api:*:*:${aws_api_gateway_rest_api.API.id}/*/${aws_api_gateway_method.Method.http_method}${aws_api_gateway_resource.Resource.path}"
+  source_arn = "arn:${data.aws_partition.current.partition}:execute-api:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.API.id}/*/${aws_api_gateway_method.Method.http_method}${aws_api_gateway_resource.Resource.path}"
+
   depends_on = [aws_api_gateway_integration.Integration]
 }
 
